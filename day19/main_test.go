b31cb874f59bc7a8d9e8f7d5e6230af9c72aeb9f
Eric Lunt <eric@lunt.software>
2025-11-25 14:15:35 -0600

    Get rid of a bad test

--- day19/main_test.go
+++ day19/main_test.go
@@ -26,23 +26,3 @@ func Test_part1(t *testing.T) {
 		})
 	}
 }
-
-func Test_part2(t *testing.T) {
-	type args struct {
-		entries []string
-	}
-	tests := []struct {
-		name string
-		args args
-		want uint64
-	}{
-		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 31},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := part2(tt.args.entries); got != tt.want {
-				t.Errorf("part2() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
